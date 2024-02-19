a1c8b875afe8631cbb898e369bab851819e66a2a
Greg Atemi <magrega7180@gmail.com>
2024-02-19 22:17:35 +0300

    Optimizing unit test

--- app/jaythree/settings.py
+++ app/jaythree/settings.py
@@ -88,17 +88,17 @@ WSGI_APPLICATION = 'jaythree.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': "postgres",
+        'HOST' : "db",
+        'PASSWORD': "postgres",
+        'PORT': 5432
+    },
+    '_test': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': "postgres",
-    #     'USER': "postgres",
-    #     'HOST' : "db",
-    #     'PASSWORD': "postgres",
-    #     'PORT': 5432,
-    # }
 }
 
 # DATABASES = {
