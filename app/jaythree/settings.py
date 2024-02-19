d02ae4df300d0e51ab43d94c5d5d73a836a36d04
Greg Atemi <magrega7180@gmail.com>
2024-02-20 00:33:19 +0300

    Optimizing unit test

--- app/jaythree/settings.py
+++ app/jaythree/settings.py
@@ -94,19 +94,15 @@ DATABASES = {
         'HOST' : "db",
         'PASSWORD': "postgres",
         'PORT': 5432
-    },
-    'test_database': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
