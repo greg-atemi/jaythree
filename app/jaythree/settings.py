da7d022b81f778925d788821487526c4eceb81ab
Greg Atemi <magrega7180@gmail.com>
2024-02-19 22:09:03 +0300

    Optimizing unit test

--- app/jaythree/settings.py
+++ app/jaythree/settings.py
@@ -98,6 +98,10 @@ DATABASES = {
         # 'NAME': os.environ.get("DB_NAME"),
         # 'USER': os.environ.get("DB_USER"),
         # 'PASSWORD': os.environ.get("DB_PASS"),
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
