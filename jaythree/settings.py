c26478d44d27a3ec8f6aa11b5de6f5267b484256
gregatemi <greg.atemi@systechafrica.com>
2024-08-24 10:31:51 +0300

    Reverting back to sqlite dev database

--- jaythree/settings.py
+++ jaythree/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'jaythree.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "postgres",
-        'USER': "postgres",
-        'PASSWORD': "postgres",
-        'HOST': "localhost",
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': "jaythree",
+#         'USER': "greg",
+#         'PASSWORD': "azerty254",
+#         'HOST': "localhost",
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
